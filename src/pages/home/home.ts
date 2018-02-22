a03931d633d283983771920f7057e38571b31d69
Patrick Vieira Feitosa <31283687+patrickfeitosa@users.noreply.github.com>
2018-02-22 11:55:33 -0300

    Melhoria do swiper-page
    Melhoria do swiper-page e implementação de semântica no código do slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { ViewChild } from '@angular/core';
-import { Slides } from 'ionic-angular';
-
 import { ServiceProvider} from '../../providers/service/service';
 
 @Component({
@@ -12,24 +9,11 @@ import { ServiceProvider} from '../../providers/service/service';
 })
 export class HomePage {
 
-	@ViewChild(Slides) slides: Slides;
-
   	gallery: any[];
 
   	constructor(public navCtrl: NavController, public service: ServiceProvider) {
   		this.getData();
-  	}
-
-  	//Tratativa do Slide
-  	goToSlide() {
-    	this.slides.slideTo(2, 500);
-  	}
-
-  	slideChanged() {
-    	let currentIndex = this.slides.getActiveIndex();
-    	console.log('Current index is', currentIndex);
-  	}
-	
+  	}	
 
   	//Tratativa dos Dados
   	getData(){
